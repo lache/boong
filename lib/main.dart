3c9504561e53c3234be2a56d3fba943b503e89ff
Geoyeob Kim <gasbank@gmail.com>
2023-12-10 03:00:13 +0900

    audioplayer 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:boong/game_manager.dart';
 import 'package:boong/game_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+final audioPlayer = AudioPlayer();
 
 void main() {
   runApp(
